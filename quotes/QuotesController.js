abe158cb965d7dc487dad5fadcb1c84abd54ee1d
Lucas Zapico <lucaszapico@luca-macbook.local>
2020-08-13 17:26:38 -0700

    enhance CRUD operations to promises

--- quotes/QuotesController.js
+++ quotes/QuotesController.js
@@ -1,75 +1,58 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import Quote from './Quote.js'
+import express from "express";
+import bodyParser from "body-parser";
+import Quote from "./Quote.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // router.use(bodyParser.urlencoded({
 //   extended: true
 // }))
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
-router.get('/', (req, res, next) => {
-  Quote.find({},(err, quotes) => {
-    if(err){
-      next(err)
-    } else {
-      res.status(200).send(quotes)
-    }
-  })  
- })
+router.get("/", (req, res, next) => {
+  Quote.find({})
+    .then((quotes) => {
+      res.status(200).send(quotes);
+    })
+    .catch((err) => next(err));
+});
 
- router.get('/:id', (req, res, next) => {
-  Quote.findById(req.params.id,(err, quotes) => {
-    if(err){
-      next(err)
-    } else {
-      res.status(200).send(quotes)
-    }
-  })  
- })
+router.get("/:id", (req, res, next) => {
+  Quote.findById(req.params.id)
+    .then((quote) => {
+      res.status(200).send(quote);
+    })
+    .catch((err) => next(err));
+});
 
 router.post(`/`, (req, res, next) => {
   const newQuote = {
-    author: req.body.author, 
+    author: req.body.author,
     categories: req.body.categories,
     quote: req.body.quote,
     notes: req.body.notes,
-  }
-  Quote.create(newQuote,(err, quote) => {
-    if(err){
-      next(err)
-    } else {
-      res.status(200).send(quote)
-    }
-  })
-})
+  };
+  Quote.create(newQuote)
+    .then((quote) => res.status(200).send(quote))
+    .catch((err) => next(err));
+});
 
 router.put(`/:id`, (req, res, next) => {
   const quoteUpdate = {
-    author: req.body.author, 
+    author: req.body.author,
     categories: req.body.categories,
     quote: req.body.quote,
     notes: req.body.notes,
-  }
-  Quote.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, quote) => {
-    if(err){
-      next(err)
-    } else {
-      res.status(200).send(quote)
-    }
-  })
-})
+  };
+  Quote.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((quote) => res.status(200).send(quote))
+    .catch((err) => next(err));
+});
 
-router.delete('/:id', (req, res, next)=> {
-  Quote.findByIdAndRemove(req.params.id, (err, quote) => {
-    if(err) {
-      next(err)
-    } else {
-      res.status(200).send('quote was delted')
-    }
-  })
-})
-
+router.delete("/:id", (req, res, next) => {
+  Quote.findByIdAndRemove(req.params.id)
+    .then((quote) => res.status(200).send(`Quote id ${quote._id} was deleted`))
+    .catch((err) => next(err));
+});
 
 export default router;
